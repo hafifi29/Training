527263270657df03f4a645e7d1f03922865e4916
Mohamed Adel <113151084+MohamedAdel86@users.noreply.github.com>
2023-03-26 00:15:41 +0200

    HTML and form edits

--- training/forms.py
+++ training/forms.py
@@ -1,9 +1,41 @@
 from django import forms
-
-class addNominee(forms.Form):
+from .models import Nominee_user, Vote
+class NomineeForm(forms.ModelForm):
+    class Meta:
+        model = Nominee_user
+        fields = [
+            'Name',
+            'phone_no',
+            'nominee_id',
+            'email',
+            'birthdate',
+            'address',
+            'collegeYear',
+            'community',
+            'rec_letter',
+        ]
 
     nominee_id = forms.CharField(max_length=6) 
     phone_no = forms.CharField(max_length=12)
-    community = forms.MultipleChoiceField(choices=['اللجنة العلمية','اللجنة الرياضية','اللجنة الاجتماعية','أسرة الجوالة و الخدمات','اللجنة الثقافية','اللجنة الفنية','لجنة الاسر و الرحلات'])
+    community = forms.ChoiceField(choices = ( ('1','اللجنة العلمية'),
+                                            ('2','اللجنة الرياضية'),
+                                            ('3','اللجنة الاجتماعية'),
+                                            ('4','أسرة الجوالة و الخدمات'),
+                                            ('5','اللجنة الثقافية'),
+                                            ('6','اللجنة الفنية'),
+                                            ('7','لجنة الاسر و الرحلات')
+                                        )
+                )
     rec_letter = forms.FileField()
     email = forms.EmailField()
+
+
+
+class VoteForm(forms.Form):
+    Sports = forms.ModelChoiceField(Nominee_user.objects.filter(community='الرياضية'))
+    Scientific = forms.ModelChoiceField(Nominee_user.objects.filter(community='العلمية'))
+    Social = forms.ModelChoiceField(Nominee_user.objects.filter(community='الاجتماعية'))
+    Scout = forms.ModelChoiceField(Nominee_user.objects.filter(community='أسرةالجوالة'))
+    Cultural = forms.ModelChoiceField(Nominee_user.objects.filter(community='الثقافية'))
+    Art = forms.ModelChoiceField(Nominee_user.objects.filter(community='الفنية'))
+    Family = forms.ModelChoiceField(Nominee_user.objects.filter(community='الاسر والرحلات'))
--- training/models.py
+++ training/models.py
@@ -2,65 +2,41 @@ from django.db import models
 from django.contrib.auth.models import User as User_
 
 # Create your models here.
-class User(models.Model):
 
-    user_id = models.CharField(max_length=7, primary_key= True)
-    SSN = models.CharField(max_length=14)
-    name = models.CharField(max_length = 20)
-    password = models.CharField(max_length = 20)
-    year = models.CharField(max_length=10)
-    administrator = models.BooleanField()
+class Nominee_user(models.Model):
 
-    def __str__(self):
-        return str (self.user_id)
-    
-class Nominee(models.Model):
-
-    nominee_id = models.ForeignKey(User, on_delete= models.CASCADE)  
+    Name = models.CharField(max_length=50, default="")
     phone_no = models.CharField(max_length=12)
+    nominee_id = models.ForeignKey(User_, on_delete= models.CASCADE)
+    email = models.EmailField(max_length=50)
+    address = models.CharField(max_length=200, null=True)
+    birthdate = models.DateField(null=True)
     community = models.CharField(max_length=20)
-    position = models.CharField(max_length=20)
-    rec_letter = models.BinaryField()
-    email = models.CharField(max_length=50)
-    final_list = models.BooleanField()
-
+    collegeYear = models.IntegerField(max_length=20,null=True)
+    rec_letter = models.FileField()
+    final_list = models.BooleanField(default=False)
+    Numofvotes = models.IntegerField(default=0)
 
     def __str__(self):
         return str (self.nominee_id)
-    
 
-class Voting(models.Model):
+
+class Vote(models.Model):
 
     voting_id = models.IntegerField()
-    voter_id = models.ForeignKey(User, on_delete= models.CASCADE)
-    nominee_id = models.ForeignKey(Nominee, on_delete= models.CASCADE)
-
+    voter_id = models.ForeignKey(User_, on_delete= models.CASCADE)
+    nominee_id = models.ForeignKey(Nominee_user, on_delete= models.CASCADE)
 
     def __str__(self):
         return str (self.voting_id)
     
-
 class Contention(models.Model):
 
     contention_id = models.IntegerField()
-    user_id = models.ForeignKey(User, on_delete= models.CASCADE)
-    nominee_id = models.ForeignKey(Nominee, on_delete= models.CASCADE)
+    user_id = models.ForeignKey(User_, on_delete= models.CASCADE)
+    nominee_id = models.ForeignKey(Nominee_user, on_delete= models.CASCADE)
     reason = models.TextField()
-
-
+    
     def __str__(self):
         return str (self.contention_id)
     
-
-class Nominee_user(models.Model):
-
-    nominee_id = models.OneToOneField(User_,on_delete=models.CASCADE) 
-    phone_no = models.CharField(max_length=12)
-    community = models.CharField(max_length=20)
-    position = models.CharField(max_length=20)
-    rec_letter = models.BinaryField()
-    email = models.CharField(max_length=50)
-    final_list = models.BooleanField()
-    
-    def __str__(self):
-        return str (self.nominee_id)
