cc8ccd814a90c13298f4a162fa7acc0652c8906a
Mahmoud Hassan <mahmoudbus7@gmail.com>
2023-04-13 22:04:52 +0200

    change website style

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATIC_DIRS = [os.path.join(BASE_DIR,'training/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
