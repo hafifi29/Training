6c16dba14875b7170ca7fd43dd00ee7ba87f7521
Mohamed Adel <113151084+MohamedAdel86@users.noreply.github.com>
2023-05-04 11:03:09 +0300

    Pythonanywhere configuration

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zbr0wtv8(gh4-u4zl9c^5mk86)r(62(!+ptf@ko#)umzzn8a=)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mohamedade452.pythonanywhere.com']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_DIRS = BASE_DIR
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
